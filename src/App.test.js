0d1fde8e0088dd2a8a9089e436df0caec9d135a4
pyksoft <pyksoft@gmail.com>
2016-12-15 12:43:53 +0100

    working game

---
+++ src/App.test.js
@@ -0,0 +1,10 @@
+import chai, { expect } from 'chai'
+import App from './App'
+
+describe('App', () => {
+  const app = new App()
+
+  it('creates 50 balloons', () => {
+    expect(app.props.balloons.length).to.eq(50)
+  })
+})
